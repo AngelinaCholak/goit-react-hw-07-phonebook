cdf357b2470a3f8cc149004c3dbcac521bacb1e6
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-11-29 12:33:40 +0400

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,37 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+  
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import { contactsReducer } from './contact/contact.reducer';
+
+// const contactsConfig = {
+//   key: 'contacts',
+//   storage,
+//   whitelist: ['contacts'],
+//   // blacklist: ['isLoading', 'error'],
+// };
+
+// export const store = configureStore({
+//   reducer: {
+//     contactsStore: persistReducer(contactsConfig, contactsReducer),
+//   },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+// export const persistor = persistStore(store);
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contact/contact.reducer';
 
@@ -5,5 +39,4 @@ export const store = configureStore({
   reducer: {
     contactsStore: contactsReducer,
   },
-
 });
